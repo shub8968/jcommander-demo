47ce897bdf3dd176f2c05072885dbc531f4df30d
Shubham Agrawal <shub8968@gmail.com>
2023-10-18 16:23:56 +0530

    first commit

---
+++ src/main/java/fish/payara/Main.java
@@ -0,0 +1,25 @@
+package fish.payara;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+public class Main {
+    @Parameter(names={"--length", "-l"})
+    int length;
+    @Parameter(names={"--pattern", "-p"})
+    int pattern;
+
+    public static void main(String ... argv) {
+        Main main = new Main();
+        JCommander.newBuilder()
+                .addObject(main)
+                .build()
+                .parse(argv);
+        main.run();
+    }
+
+    public void run() {
+        System.out.printf("%d", length);
+        System.out.printf("%d", pattern);
+    }
+}
